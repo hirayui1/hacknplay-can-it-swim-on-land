fbfa3d9d1f6411ca48c618e6392ec8ebe198857d
Monandszy <monandszy@tuta.io>
2025-10-24 12:52:29 +0200

    simplespringtemplate, starting terraform

---
+++ backend/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    application
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+java.toolchain.languageVersion = JavaLanguageVersion.of(21)
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-freemarker")
+    implementation("org.springframework.boot:spring-boot-starter-jersey")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
+    implementation("org.springframework.session:spring-session-core")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
